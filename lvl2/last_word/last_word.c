796a118dfdbd37045c4a5410ff91d40a1580e396
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2024-10-07 02:46:25 +0100

    some exercises

--- lvl2/last_word/last_word.c
+++ lvl2/last_word/last_word.c
@@ -1,36 +1,33 @@
-#include <stdio.h>
 #include <unistd.h>
 
 void last_word(char *str)
 {
-	if(!str)
-		return ;
-	int len = 0;
-	while(str[len])
-	{
-		len++;
-	}
-	int i = len - 1;
-	while(str[i] && (str[i] == ' ' || str[i] == '\t'))
-		i--;
-	int end = i;
-	while(str[i] && (str[i] != ' ' && str[i] != '\t'))
-		i--;
-	int start = ++i;
-	while(start <= end)
-	{
-		write(1, &str[start], 1);
-		start++;
-	}
-	write(1, "\n", 1);
+    if(!str)
+        return ;
+    int len = 0;
+    while(str[len])
+        len++;
+    int i = --len; // sair do NULL BYTE  
+    while(str[i] && (str[i] == ' ' || str[i] == '\t'))
+        i--;
+    int end = i;
+    while(str[i] && (str[i] != ' ' && str[i] != '\t'))
+        i--;
+    int start = ++i; // passar a frente do espaco
+    while(start <= end)
+    {
+        write(1, &str[start], 1);
+        start++;
+    }
+    write(1, "\n", 1);
 }
 
 
 
 int main(int argc, char *argv[])
 {
-	if(argc == 2)
-		last_word(argv[1]);
-	else
-		write(1, "\n", 1);
+    if(argc == 2)
+        last_word(argv[1]);
+    else
+        write(1, "\n", 1);
 }

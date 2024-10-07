06f099d302ef1208b51d0392f94dd15554677274
Rafael Joao Amorim De Matos <raamorim@c2r4s10.42lisboa.com>
2024-10-07 14:39:27 +0100

    some exercises

---
+++ lvl2/ft_strcmp/ft_strcmp.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int    ft_strcmp(char *s1, char *s2)
+{
+	if(!s1 || !s2)
+		return (-1);
+	int i = 0;
+	while(s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
